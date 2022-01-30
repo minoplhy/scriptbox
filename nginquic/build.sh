2aa081cccae458a58bb80307588b561538245a8d
unknown <hej@aspoblourib.1w1.one>
2022-01-30 20:48:39 +0700

    nginquic : link curl

--- nginquic/build.sh
+++ nginquic/build.sh
@@ -1,5 +1,5 @@
 cd ~/
-curl -sSL packages.sh | bash
+curl -sSL https://raw.githubusercontent.com/minoplhy/script/main/nginquic/packages.sh | bash
 mkdir nginquic && cd nginquic
 hg clone -b quic https://hg.nginx.org/nginx-quic
 git clone https://github.com/google/boringssl
@@ -7,9 +7,9 @@ cd boringssl
 mkdir build && cd build && cmake .. && make
 cd .. && cd ..
 cd nginx-quic
-mkdir mosc && cd mosc && curl dependency.sh | bash && cd ..
-curl -sSL configure.sh | bash && make
+mkdir mosc && cd mosc && curl -sSL https://raw.githubusercontent.com/minoplhy/script/main/nginquic/modules.sh | bash && cd ..
+curl -sSL https://raw.githubusercontent.com/minoplhy/script/main/nginquic/configure.sh | bash && make
 cd objs cp *.so /lib/nginx/modules
 cp nginx /usr/sbin/nginx
-curl modules.conf > modules.conf
+curl -sSL https://raw.githubusercontent.com/minoplhy/script/main/nginquic/modules.conf > modules.conf
 cp modules.conf /etc/nginx/modules-enabled
