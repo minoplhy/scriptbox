39ed37120a957a6536bf0a95d627e2b7938232b9
unknown <hej@aspoblourib.1w1.one>
2022-01-31 19:27:39 +0700

    nginquic : add geoip Library modules

--- nginquic/packages.sh
+++ nginquic/packages.sh
@@ -1,1 +1,1 @@
-sudo apt-get install mercurial golang libunwind-dev libpcre3 libpcre3-dev zlib1g-dev cmake make libxslt1-dev libgd-dev libssl-dev libperl-dev libpam0g-dev mercurial git -y
+sudo apt-get install mercurial golang libunwind-dev libpcre3 libpcre3-dev zlib1g-dev cmake make libxslt1-dev libgd-dev libssl-dev libperl-dev libpam0g-dev libgeoip-dev git -y
