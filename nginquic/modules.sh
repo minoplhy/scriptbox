d8a172c33102579357f4224aad8fbd7b5178dbee
unknown <hej@aspoblourib.1w1.one>
2022-06-11 20:22:54 +0700

    nginquic : Moved to dedicated repository

--- nginquic/modules.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/openresty/headers-more-nginx-module
-git clone https://github.com/sto/ngx_http_auth_pam_module
-git clone https://github.com/arut/nginx-dav-ext-module/
-git clone https://github.com/openresty/echo-nginx-module
-git clone https://github.com/masterzen/nginx-upload-progress-module
-git clone https://github.com/nginx-modules/ngx_cache_purge
--- nginquic/packages.sh
+++
@@ -1,1 +0,0 @@
-sudo apt-get install mercurial golang libunwind-dev libpcre3 libpcre3-dev zlib1g-dev cmake make libxslt1-dev libgd-dev libssl-dev libperl-dev libpam0g-dev libgeoip-dev git -y
