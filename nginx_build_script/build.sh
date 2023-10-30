b92430704de3aba766877c21bb1c9548eb87ed17
minoplhy <c@3qx.nl>
2023-10-31 03:44:50 +0700

    Revert "nginx_build_script : re-init compile w/ lua"
    This reverts commit 0cf3d8613af20d5a5bdb14ecdbccdb602689ad5b.

--- nginx_build_script/build.sh
+++ nginx_build_script/build.sh
@@ -30,20 +30,21 @@ git submodule update
 make
 sudo make install
 
+# Whoops! Openresty doesn't support QUIC yet!
 # lua-nginx-module buildup part (Big Part)
 #
-mkdir $HOMEDIRECTORY/nginx-lua && cd $HOMEDIRECTORY/nginx-lua
-mkdir -p /opt/nginx-lua-module/
-git clone https://github.com/openresty/lua-resty-core
-git clone https://github.com/openresty/lua-resty-lrucache
-git clone https://github.com/openresty/luajit2
-
-cd luajit2 && make install PREFIX=/opt/nginx-lua-module/luajit2 && cd ..
-cd lua-resty-core && make install PREFIX=/opt/nginx-lua-module/ && cd ..
-cd lua-resty-lrucache && make install PREFIX=/opt/nginx-lua-module/ && cd ..
-
-export LUAJIT_LIB=/opt/nginx-lua-module/luajit2/lib
-export LUAJIT_INC=/opt/nginx-lua-module/luajit2/include/luajit-2.1
+## mkdir $HOMEDIRECTORY/nginx-lua && cd $HOMEDIRECTORY/nginx-lua
+## mkdir -p /opt/nginx-lua-module/
+## git clone https://github.com/openresty/lua-resty-core
+## git clone https://github.com/openresty/lua-resty-lrucache
+## git clone https://github.com/openresty/luajit2
+##
+## cd luajit2 && make install PREFIX=/opt/nginx-lua-module/luajit2 && cd ..
+## cd lua-resty-core && make install PREFIX=/opt/nginx-lua-module/ && cd ..
+## cd lua-resty-lrucache && make install PREFIX=/opt/nginx-lua-module/ && cd ..
+## ##
+## export LUAJIT_LIB=/opt/nginx-lua-module/luajit2/lib
+## export LUAJIT_INC=/opt/nginx-lua-module/luajit2/include/luajit-2.1
 
 # Build Nginx
 
